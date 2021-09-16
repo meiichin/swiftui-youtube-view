e53e4f22d1a80daa100c837cbb8f0800fc1474f6
meiichin <meiichan3017@gmail.com>
2021-09-16 22:29:55 +0700

    Initial Commit

---
+++ YouTubeView/YouTubeViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  YouTubeViewApp.swift
+//  YouTubeView
+//
+//  Created by Yuu on 16/09/21.
+//
+
+import SwiftUI
+
+@main
+struct YouTubeViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
