cbca1361b2016e82b997914ec42dc151f20c4ec8
meiichin <meiichan3017@gmail.com>
2021-09-16 22:54:00 +0700

    [Added] leading navigation [Added] trailing navigation

--- YouTubeView/ContentView.swift
+++ YouTubeView/ContentView.swift
@@ -9,13 +9,53 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        Home()
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+    }
+}
+
+struct Home: View {
+    var body: some View{
+        NavigationView{
+            Text("Hallo Dev")
+            
+            //navigasi atas
+            .navigationBarItems(
+                leading:
+                    HStack{
+                        Image("youtube")
+                            .renderingMode(.original)
+                            .resizable()
+                            .frame(width: 90, height: 20)
+                    },
+                trailing:
+                    HStack(spacing:30){
+                        Button(action: {print("button 1")}){
+                            Image(systemName: "tray.full")
+                                .foregroundColor(Color.secondary)
+                        }
+                        Button(action: {print("button 2")}){
+                            Image(systemName: "video.fill")
+                                .foregroundColor(Color.secondary)
+                        }
+                        Button(action: {print("button 3")}){
+                            Image(systemName: "magnifyingglass")
+                                .foregroundColor(Color.secondary)
+                        }
+                        Button(action: {print("button 4")}){
+                            Image("me")
+                                .renderingMode(.original)
+                                .resizable()
+                                .frame(width: 20, height: 20)
+                                .clipShape(/*@START_MENU_TOKEN@*/Circle()/*@END_MENU_TOKEN@*/)
+                        }
+                    }
+            )
+        }
     }
 }
