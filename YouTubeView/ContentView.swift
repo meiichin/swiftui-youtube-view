7bac66298a856ac9751acb1f1e5f4d6ed5e3d407
meiichin <meiichan3017@gmail.com>
2021-09-17 12:58:19 +0700

    [Added] tab view

--- YouTubeView/ContentView.swift
+++ YouTubeView/ContentView.swift
@@ -9,7 +9,37 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Home()
+        ZStack{
+            TabView{
+                Home()
+                    .tabItem {
+                        Image(systemName: "house.fill")
+                        Text("Beranda")
+                    }
+                Home()
+                    .tabItem {
+                        Image(systemName: "paperplane.fill")
+                        Text("Exploration")
+                    }
+                Home()
+                    .tabItem {
+                        Image(systemName: "tray.fill")
+                        Text("Subscription")
+                    }
+                Home()
+                    .tabItem {
+                        Image(systemName: "envelope.fill")
+                        Text("Inbox")
+                    }
+                
+                Home()
+                    .tabItem {
+                        Image(systemName: "play.rectangle.fill")
+                        Text("Collection")
+                    }
+            }
+            .accentColor(Color.red)
+        }
     }
 }
 
