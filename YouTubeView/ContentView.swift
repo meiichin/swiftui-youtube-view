811a9c5209c92e9d1ab53db8df749ad796de7a7e
meiichin <meiichan3017@gmail.com>
2021-09-17 12:49:48 +0700

    [Added] stuct for content

--- YouTubeView/ContentView.swift
+++ YouTubeView/ContentView.swift
@@ -22,40 +22,144 @@ struct ContentView_Previews: PreviewProvider {
 struct Home: View {
     var body: some View{
         NavigationView{
-            Text("Hallo Dev")
-            
-            //navigasi atas
-            .navigationBarItems(
-                leading:
-                    HStack{
-                        Image("youtube")
-                            .renderingMode(.original)
-                            .resizable()
-                            .frame(width: 90, height: 20)
-                    },
-                trailing:
-                    HStack(spacing:30){
-                        Button(action: {print("button 1")}){
-                            Image(systemName: "tray.full")
-                                .foregroundColor(Color.secondary)
-                        }
-                        Button(action: {print("button 2")}){
-                            Image(systemName: "video.fill")
-                                .foregroundColor(Color.secondary)
-                        }
-                        Button(action: {print("button 3")}){
-                            Image(systemName: "magnifyingglass")
-                                .foregroundColor(Color.secondary)
-                        }
-                        Button(action: {print("button 4")}){
-                            Image("me")
+            Content()
+                
+                //navigasi atas
+                .navigationBarItems(
+                    leading:
+                        HStack{
+                            Image("youtube")
                                 .renderingMode(.original)
                                 .resizable()
-                                .frame(width: 20, height: 20)
-                                .clipShape(/*@START_MENU_TOKEN@*/Circle()/*@END_MENU_TOKEN@*/)
+                                .frame(width: 90, height: 20)
+                        },
+                    trailing:
+                        HStack(spacing:30){
+                            Button(action: {print("button 1")}){
+                                Image(systemName: "tray.full")
+                                    .foregroundColor(Color.secondary)
+                            }
+                            Button(action: {print("button 2")}){
+                                Image(systemName: "video.fill")
+                                    .foregroundColor(Color.secondary)
+                            }
+                            Button(action: {print("button 3")}){
+                                Image(systemName: "magnifyingglass")
+                                    .foregroundColor(Color.secondary)
+                            }
+                            Button(action: {print("button 4")}){
+                                Image("me")
+                                    .renderingMode(.original)
+                                    .resizable()
+                                    .frame(width: 20, height: 20)
+                                    .clipShape(Circle())
+                            }
                         }
+                )
+                .navigationBarTitle("", displayMode: .inline)
+                
+        }.navigationViewStyle(StackNavigationViewStyle())
+    }
+}
+
+
+struct Content: View {
+    var body: some View{
+        List{
+            // Content 1
+            VStack{
+                ZStack(alignment:.bottomTrailing){
+                    Image("c1")
+                        .resizable()
+                        .aspectRatio(contentMode: /*@START_MENU_TOKEN@*/.fill/*@END_MENU_TOKEN@*/)
+                    
+                    Text("10:00")
+                        .padding(.all,5)
+                        .foregroundColor(Color.white)
+                        .font(.caption)
+                        .background(Color.black)
+                        .cornerRadius(5)
+                        .padding(.trailing,5)
+                        .padding(.bottom, 5)
+                }
+                HStack(spacing:20){
+                    Image("me")
+                        .resizable()
+                        .frame(width:30, height: 30)
+                        .clipShape(/*@START_MENU_TOKEN@*/Circle()/*@END_MENU_TOKEN@*/)
+                    VStack(alignment:.leading){
+                        Text("Tutorial SwiftUI #1").font(.headline)
+                        Text("Ini adalah deskripsi video - 300x ditonton - 10 jam yang lalu").font(.caption)
                     }
-            )
+                    Spacer()
+                    Image(systemName: "list.bullet")
+                }
+                
+            }
+            
+            // Content 2
+            VStack{
+                ZStack(alignment:.bottomTrailing){
+                    Image("c2")
+                        .resizable()
+                        .aspectRatio(contentMode: /*@START_MENU_TOKEN@*/.fill/*@END_MENU_TOKEN@*/)
+                    
+                    Text("10:00")
+                        .padding(.all,5)
+                        .foregroundColor(Color.white)
+                        .font(.caption)
+                        .background(Color.black)
+                        .cornerRadius(5)
+                        .padding(.trailing,5)
+                        .padding(.bottom, 5)
+                }
+                HStack(spacing:20){
+                    Image("me")
+                        .resizable()
+                        .frame(width:30, height: 30)
+                        .clipShape(/*@START_MENU_TOKEN@*/Circle()/*@END_MENU_TOKEN@*/)
+                    VStack(alignment:.leading){
+                        Text("Tutorial SwiftUI #1").font(.headline)
+                        Text("Ini adalah deskripsi video - 300x ditonton - 10 jam yang lalu").font(.caption)
+                    }
+                    Spacer()
+                    Image(systemName: "list.bullet")
+                }
+                
+            }
+            
+            // Content 3
+            VStack{
+                ZStack(alignment:.bottomTrailing){
+                    Image("c1")
+                        .resizable()
+                        .aspectRatio(contentMode: /*@START_MENU_TOKEN@*/.fill/*@END_MENU_TOKEN@*/)
+                    
+                    Text("10:00")
+                        .padding(.all,5)
+                        .foregroundColor(Color.white)
+                        .font(.caption)
+                        .background(Color.black)
+                        .cornerRadius(5)
+                        .padding(.trailing,5)
+                        .padding(.bottom, 5)
+                }
+                HStack(spacing:20){
+                    Image("me")
+                        .resizable()
+                        .frame(width:30, height: 30)
+                        .clipShape(/*@START_MENU_TOKEN@*/Circle()/*@END_MENU_TOKEN@*/)
+                    VStack(alignment:.leading){
+                        Text("Tutorial SwiftUI #1").font(.headline)
+                        Text("Ini adalah deskripsi video - 300x ditonton - 10 jam yang lalu").font(.caption)
+                    }
+                    Spacer()
+                    Image(systemName: "list.bullet")
+                }
+                
+            }
+            
+            
         }
     }
 }
